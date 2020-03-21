422524b28a4ca54f3783d3c7aee225028d0fd23d
Guilherme-Freschi <50874403+Guilherme-Freschi@users.noreply.github.com>
2020-03-20 21:14:22 -0300

    Update GerenciadorDeComissao.java

--- src/aula20200320/testesGerais/GerenciadorDeComissao.java
+++ src/aula20200320/testesGerais/GerenciadorDeComissao.java
@@ -7,3 +7,33 @@ public class GerenciadorDeComissao {
 	}
 
 }
+
+
+public static double calcularComissaoComValorMinimo(double valorMinimo, double valorDaVenda, double percentualDeComissao) {
+		double padrao = valorDaVenda * (percentualDeComissao/100);
+		
+		if (padrao < valorMinimo) {
+			return valorMinimo;
+		}
+		return padrao;
+	}
+public static double calcularComissaoComValorMinimo(double valorMinimo, double valorDaVenda, double percentualDeComissao) {
+		double padrao = valorDaVenda * (percentualDeComissao/100);
+		
+		if (padrao < valorMinimo) {
+			return valorMinimo;
+		}
+		return padrao;
+	}
+public static double calcularComissaoComFaixa(double valorMinimo, double valorMaximo, double valorDaVenda, double percentualDeComissao) {
+		double padrao = valorDaVenda * (percentualDeComissao/100);
+		
+		if (padrao < valorMinimo) {
+			return valorMinimo;
+		}
+		if (padrao > valorMaximo) {
+			return valorMaximo;
+		}
+		return padrao;
+	}
+
