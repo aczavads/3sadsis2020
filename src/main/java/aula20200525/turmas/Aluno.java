65ea365dcc63800d809f8a02344c64cf3380407a
arthur.zavadski <aczavads@gmail.com>
2020-05-25 19:43:16 -0300

    Exemplo de País e Estado.

---
+++ src/main/java/aula20200525/turmas/Aluno.java
@@ -0,0 +1,20 @@
+package aula20200525.turmas;
+
+public class Aluno {
+	private String nome;
+	private String ra;
+	
+	public Aluno(String nome, String ra) {
+		this.nome = nome;
+		this.ra = ra;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getRa() {
+		return ra;
+	}
+
+}
